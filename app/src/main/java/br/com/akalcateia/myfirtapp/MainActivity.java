00372f49e889c521d3422f271483fdc75ed4b0a6
Roger <rogerio.reaper@gmail.com>
2020-03-14 16:19:05 -0300

    Projeto completo

---
+++ app/src/main/java/br/com/akalcateia/myfirtapp/MainActivity.java
@@ -0,0 +1,29 @@
+package br.com.akalcateia.myfirtapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "br.com.akalcateia.myfirtapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        //Toast.makeText(this, "Botão pressionado", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
